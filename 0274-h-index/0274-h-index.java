0bdb771ee9fae8999e35527c426604b5646b5869
Rajender Thota <java.rajender@gmail.com>
2024-09-21 09:29:17 -0400

    0274-H-INDEX Stats: Time: 0 ms (100%), Space: 41 MB (98.37%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -12,15 +12,40 @@ class Solution {
     // return 0;
 
 
-     int n = citations.length;
-    Arrays.sort(citations);
+    //  int n = citations.length;
+    // Arrays.sort(citations);
     
-    for (int i = 0; i < n; i++) {
-        if (citations[i] >= n - i) {
-            return n - i;
+    // for (int i = 0; i < n; i++) {
+    //     if (citations[i] >= n - i) {
+    //         return n - i;
+    //     }
+    // }
+    
+    // return 0;
+
+//below counting sort implementation
+
+    int n = citations.length;
+    int[] count = new int[n + 1];
+
+    // Counting Sort
+    for (int citation : citations) {
+        if (citation >= n) {
+            count[n]++;
+        } else {
+            count[citation]++;
         }
     }
-    
+
+    int sum = 0;
+    for (int i = n; i >= 0; i--) {
+        sum += count[i];
+        if (sum >= i) {
+            return i;
+        }
+    }
+
     return 0;
+
     }
 }
