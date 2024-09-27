64fade7bcbb3de48399e2296c7df011faa6a5384
Rajender Thota <java.rajender@gmail.com>
2024-09-26 21:03:34 -0400

    0028-FIND-THE-INDEX-OF-THE-FIRST-OCCURRENCE-IN-A-STRING Stats: Time: 0 ms (100%), Space: 41.1 MB (94.87%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,45 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
+
+
+//two pointer technique
+
+public int strStr(String haystack, String needle) {
+        int haystackLength = haystack.length();
+        int needleLength = needle.length();
+
+        if (needleLength == 0) {
+            return 0;
+        }
+
+        // Two pointers: one for haystack and one for needle
+        int i = 0; // Pointer for haystack
+        int j = 0; // Pointer for needle
+
+        while (i < haystackLength) {
+            // If characters match, move both pointers
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+
+                // If j has reached the end of needle, we found the match
+                if (j == needleLength) {
+                    return i - j; // The start of the match
+                }
+            } else {
+                // If there's a mismatch, reset j and adjust i
+                i = i - j + 1; // Move i back to the next character after the mismatch
+                j = 0;         // Reset j to start of needle
+            }
+        }
+
+        // If no match is found, return -1
+        return -1;
+    }
+
+
+
+    //brueforce implementation
+    public int strStr1(String haystack, String needle) {
 
         int l=0,r=0,stl=haystack.length(),nl=needle.length();
 
