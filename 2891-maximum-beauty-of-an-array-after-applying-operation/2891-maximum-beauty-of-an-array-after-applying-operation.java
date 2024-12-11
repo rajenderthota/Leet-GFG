159f4c7dc1b0330dd05d4904b0625a14058b2198
Rajender Thota <java.rajender@gmail.com>
2024-12-11 08:31:15 -0500

    2891-MAXIMUM-BEAUTY-OF-AN-ARRAY-AFTER-APPLYING-OPERATION Stats: Time: 12 ms (89.01%), Space: 57.9 MB (93.56%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,5 +1,55 @@
 class Solution {
-    public int maximumBeauty(int[] nums, int k) {
+
+
+    public int maximumBeauty(int []flowers, int additions){
+
+          // Calculate the maximum value after all possible additions.
+
+        // We add extra space for fluctuations in the count within the k range.
+
+        int maxValue = Arrays.stream(flowers).max().getAsInt() + additions * 2 + 2;
+
+        int[] delta = new int[maxValue];
+
+      
+
+        // Build the difference array using the number of additions possible adding and subtracting at the range ends.
+
+        for (int flower : flowers) {
+
+            delta[flower]++;
+
+            delta[flower + additions * 2 + 1]--;
+
+        }
+
+      
+
+        int maxBeauty = 0; // This will hold the maximum beauty calculated so far.
+
+        int runningSum = 0; // This will be used to compute the running sum from the difference array.
+
+      
+
+        // Compute the running sum and find the maximum value.
+
+        for (int value : delta) {
+
+            runningSum += value;
+
+            maxBeauty = Math.max(maxBeauty, runningSum);
+
+        }
+
+      
+
+        // Return the computed maximum beauty of the bouquet.
+
+        return maxBeauty;
+    }
+
+
+    public int maximumBeauty_simple(int[] nums, int k) {
           // Sort the array to enable efficient range operations
         Arrays.sort(nums);
 
