3a0916914d96db8dbdfb0db8dbb106bb34bacdc3
Rajender Thota <java.rajender@gmail.com>
2024-12-18 06:28:08 -0500

    1570-FINAL-PRICES-WITH-A-SPECIAL-DISCOUNT-IN-A-SHOP Stats: Time: 4 ms (40.47%), Space: 43.6 MB (60.92%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,5 +1,31 @@
 class Solution {
-    public int[] finalPrices(int[] prices) {
+
+    public int[] finalPrices(int[] prices){
+
+        int n = prices.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // Find the discount for previous items
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int index = stack.pop();
+                result[index] = prices[index] - prices[i];
+            }
+            // Push the current index onto the stack
+            stack.push(i);
+        }
+
+        // For remaining items in the stack, no discount applies
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            result[index] = prices[index];
+        }
+
+        return result;
+
+    }
+    public int[] finalPrices1(int[] prices) {
 
         //brute force solution
         int n=prices.length, result[]=new int[n];
