d1fac864bcc01412f62b0402ab799349a255e1db
Rajender Thota <java.rajender@gmail.com>
2024-12-30 02:00:04 -0500

    2562-COUNT-WAYS-TO-BUILD-GOOD-STRINGS Stats: Time: 50 ms (5.37%), Space: 56.5 MB (18.46%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
@@ -4,8 +4,27 @@ class Solution {
     private HashMap<Integer, Integer> dp = new HashMap<>(); // Memoization map
 
 
+     public int countGoodStrings(int low, int high, int zero, int one) {
+        int mod = 1000000007;
+        HashMap<Integer, Integer> dp = new HashMap<>(); // Map to store length -> num of strings
+        dp.put(0, 1); // Base case: one valid string of length 0
 
-    public int countGoodStrings(int low, int high, int zero, int one) {
+        // Populate the dp map for lengths from 1 to high
+        for (int i = 1; i <= high; i++) {
+            int countZero = dp.getOrDefault(i - zero, 0);
+            int countOne = dp.getOrDefault(i - one, 0);
+            dp.put(i, (countZero + countOne) % mod);
+        }
+
+        // Sum up the counts for lengths from low to high
+        int result = 0;
+        for (int i = low; i <= high; i++) {
+            result = (result + dp.getOrDefault(i, 0)) % mod;
+        }
+
+        return result;
+    }
+    public int countGoodStrings_dp(int low, int high, int zero, int one) {
          return dfs(0, low, high, zero, one); // Start the recursion
     }
 
