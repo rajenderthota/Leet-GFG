f883275f3e6a49bf6b124b23a6ff6a2a39b8ab2f
Rajender Thota <java.rajender@gmail.com>
2025-01-01 08:30:41 -0500

    1537-MAXIMUM-SCORE-AFTER-SPLITTING-A-STRING Stats: Time: 1 ms (97.36%), Space: 41.6 MB (36.08%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,31 +1,86 @@
 class Solution {
-    public int maxScore(String s) {
-        
-       int n = s.length();
-        
-        // Count total number of '1's in the string
-        int totalOnes = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '1') totalOnes++;
+
+
+ public int maxScore(String s) {
+
+        // Initialize total score for the first partition
+
+        int totalScore = 0;
+
+        // If the first character is '0', increase the score by 1
+
+        if (s.charAt(0) == '0') {
+
+            totalScore++;
+
         }
-        
-        int maxScore = 0;
-        int leftZeros = 0;
-        int rightOnes = totalOnes;
-        
-        // Iterate through the string, keeping track of scores
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == '0') {
-                leftZeros++;
-            } else {
-                rightOnes--;
+
+        // Calculate initial score for '1's in the string, skipping the first character
+
+        for (int i = 1; i < s.length(); ++i) {
+
+            if (s.charAt(i) == '1') {
+
+                totalScore++;
+
             }
-            
-            // Calculate score for this split
-            int currentScore = leftZeros + rightOnes;
-            maxScore = Math.max(maxScore, currentScore);
+
         }
+
+        // The best score is initially set to the score from the initial partition
+
+        int maxScore = totalScore;
+
+      
+
+        // Iterate through the string to find partitions and track the highest score
+
+        for (int i = 1; i < s.length() - 1; ++i) {
+
+            // If the current character is '0', increase the total score
+
+            // If it is '1', decrease the total score
+
+            totalScore += (s.charAt(i) == '0') ? 1 : -1;
+
+            // Update the maximum score if the current total score is greater
+
+            maxScore = Math.max(maxScore, totalScore);
+
+        }
+
+        // Return the highest score found amongst all possible partitions
+
+        return maxScore;
+
+    }
+
+
+    public int maxScore1(String s) {
         
+        // length of the string
+        int n=s.length(),zeros=0,ones=0,totalOnes=0,maxScore=0;
+
+        //count total no of ones in giben string
+
+        for(int i=0;i<n;i++){
+            if(s.charAt(i) == '1'){
+                totalOnes++;
+            }
+        }
+
+        //find maximum score after splitting a string
+
+        for(int i=0;i<n-1;i++){
+                if( s.charAt(i) == '0'){
+                    zeros++;
+                }else{
+                    ones++;
+                }
+
+                maxScore=Math.max(maxScore, zeros+(totalOnes-ones));
+        }
+
         return maxScore;
     }
 }
