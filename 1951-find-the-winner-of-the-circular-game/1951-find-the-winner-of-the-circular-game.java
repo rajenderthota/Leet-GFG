1fac4d008e468eb9562f9186ce0c37b649a03c33
Rajender Thota <java.rajender@gmail.com>
2025-01-03 21:24:48 -0500

    1951-FIND-THE-WINNER-OF-THE-CIRCULAR-GAME Stats: Time: 0 ms (100%), Space: 40.5 MB (69.28%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,5 +1,19 @@
 class Solution {
-    public int findTheWinner(int n, int k) {
+
+
+
+public int findTheWinner(int n, int k) {
+    int winner = 0; // Base case: 0-based index when there's only one person
+    for (int i = 2; i <= n; i++) {
+        winner = (winner + k) % i;
+    }
+    return winner + 1; // Convert to 1-based index
+}
+
+
+
+//below code is with recursion
+    public int findTheWinner_recursion(int n, int k) {
         
         return findWinnerRecursive(n,k)+1;
     }
