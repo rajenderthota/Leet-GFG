d8af4ddbb6eec8842e8887639f0f755c6d278086
Rajender Thota <java.rajender@gmail.com>
2025-01-03 21:27:17 -0500

    1951-FIND-THE-WINNER-OF-THE-CIRCULAR-GAME Stats: Time: 44 ms (11.49%), Space: 45.1 MB (7.04%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,8 +1,33 @@
 class Solution {
 
+//using que approach
+
+ public int findTheWinner(int n, int k) {
+        // Create a queue to represent the circle of friends
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Add all friends (1 to n) to the queue
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        // Simulate the process
+        while (queue.size() > 1) {
+            // Move the first k-1 people to the back of the queue
+            for (int i = 1; i < k; i++) {
+                queue.add(queue.poll());
+            }
+            // Remove the k-th person
+            queue.poll();
+        }
+
+        // The last remaining person is the winner
+        return queue.peek();
+    }
 
 
-public int findTheWinner(int n, int k) {
+//below is iterative approach 
+public int findTheWinner_iterative(int n, int k) {
     int winner = 0; // Base case: 0-based index when there's only one person
     for (int i = 2; i <= n; i++) {
         winner = (winner + k) % i;
