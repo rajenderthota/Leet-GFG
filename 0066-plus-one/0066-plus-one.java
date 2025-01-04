53864ee9fcaaf6d160acb4639390f3b74401b9ad
Rajender Thota <java.rajender@gmail.com>
2025-01-04 06:53:29 -0500

    0066-PLUS-ONE Stats: Time: 0 ms (100%), Space: 42 MB (36.86%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,27 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        
-        int n=digits.length;
-        boolean b=false;
-        for(int i=n-1;i>=0;i--){
-            if(digits[i] < 9){
-                digits[i]+=1;
-                // return digits;
-                b=true;
-                break;
-            }else{
-                digits[i]=0;
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            } else {
+                digits[i] = 0;
             }
         }
-
-        if(b)
-        return digits;
-        else{
-            int result[]=new int[n+1];
-  result[0]=1;
-    return result;
-        }
-
-      
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
     }
 }
