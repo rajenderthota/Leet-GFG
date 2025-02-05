c7d134fe25ff9e7ead483eb486c812403b4b4b6d
Rajender Thota <java.rajender@gmail.com>
2025-02-04 21:02:14 -0500

    1915-CHECK-IF-ONE-STRING-SWAP-CAN-MAKE-STRINGS-EQUAL Stats: Time: 0 ms (100%), Space: 41.3 MB (94.96%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,5 +1,31 @@
 class Solution {
-    public boolean areAlmostEqual(String s1, String s2) {
+
+
+   public boolean areAlmostEqual(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        int n = s1.length();
+        int first = -1, second = -1;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                count++;
+                if (count > 2) return false;
+                if (first == -1) {
+                    first = i;
+                } else {
+                    second = i;
+                }
+            }
+        }
+
+        return count == 2 && s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first);
+    }
+
+    public boolean areAlmostEqual_hasmap(String s1, String s2) {
 
         HashMap<Character,Integer> s1Set=new HashMap<>();
         HashMap<Character,Integer> s2Set=new HashMap<>();
