ff06f693fb24e672c7fcab9e6c68e4352b06c1d8
Rajender Thota <java.rajender@gmail.com>
2025-02-17 09:44:27 -0500

    0215-KTH-LARGEST-ELEMENT-IN-AN-ARRAY Stats: Time: 40 ms (70.58%), Space: 57.4 MB (76.29%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -0,0 +1,106 @@
+class Solution {
+
+
+     public int findKthLargest(int[] nums, int k) {
+
+        int n=nums.length;
+        PriorityQueue<Integer> pq=new PriorityQueue<>(k);
+
+        for(int i=0;i<k;i++){
+            pq.add(nums[i]);
+        }
+
+        for(int i=k;i<n;i++){
+
+            if(pq.peek() < nums[i]){
+                pq.poll();
+                pq.add(nums[i]);
+            }
+
+        }
+
+        return pq.poll();
+     }
+
+
+    public int findKthLargest_quickSelect(int[] nums, int k) {
+        
+        return quickSelect(nums,0,nums.length-1,k);
+    }
+
+
+    // Function to find the k-th smallest element
+    public  int quickSelect(int[] arr, int low, int high, int k) {
+
+
+         // find the partition
+        int partition = partition(arr, low, high);
+
+        // if partition value is equal to the kth position,
+        // return value at k.
+        if (partition == k - 1)
+            return arr[partition];
+
+        // if partition value is less than kth position,
+        // search right side of the array.
+        else if (partition > k - 1)
+             return quickSelect(arr, low, partition - 1, k);
+
+
+        // if partition value is more than kth position,
+        // search left side of the array.
+        else
+           return quickSelect(arr, partition + 1, high, k);
+        // if (left == right) return arr[left]; // Base case
+
+        // int pivotIndex = partition(arr, left, right); // Partition the array
+
+        // if (pivotIndex == k) return arr[pivotIndex]; // Found the k-th smallest element
+        // else if (pivotIndex > k) return quickSelect(arr, left, pivotIndex - 1, k); // Search left
+        // else return quickSelect(arr, pivotIndex + 1, right, k); // Search right
+    }
+
+
+ // Lomuto Partition Scheme (chooses last element as pivot)
+    private  int partition(int[] arr, int low, int high) {
+        // int pivot = arr[right]; 
+        // int i = left; // Index for smaller elements
+
+        // for (int j = left; j < right; j++) {
+        //     if (arr[j] < pivot) { // Move smaller elements to the left
+        //         swap(arr, i, j);
+        //         i++;
+        //     }
+        // }
+        // swap(arr, i, right); // Place pivot in its correct position
+        // return i;
+
+
+        int pivot = arr[high], pivotloc = low;
+        for (int i = low; i <= high; i++) {
+            // inserting elements of less value
+            // to the left of the pivot location
+            if (arr[i] > pivot) {
+                int temp = arr[i];
+                arr[i] = arr[pivotloc];
+                arr[pivotloc] = temp;
+                pivotloc++;
+            }
+        }
+
+        // swapping pivot to the final pivot location
+        int temp = arr[high];
+        arr[high] = arr[pivotloc];
+        arr[pivotloc] = temp;
+
+        return pivotloc;
+    }
+
+
+    private void swap(int[] arr, int left, int right){
+
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+    }
+}
--- README.md
+++ README.md
@@ -17,6 +17,7 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 | [0136-single-number](https://github.com/rajenderthota/Leet-GFG/tree/master/0136-single-number) |
 | [0137-single-number-ii](https://github.com/rajenderthota/Leet-GFG/tree/master/0137-single-number-ii) |
 | [0189-rotate-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0189-rotate-array) |
+| [0215-kth-largest-element-in-an-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0215-kth-largest-element-in-an-array) |
 | [0217-contains-duplicate](https://github.com/rajenderthota/Leet-GFG/tree/master/0217-contains-duplicate) |
 | [0274-h-index](https://github.com/rajenderthota/Leet-GFG/tree/master/0274-h-index) |
 | [0349-intersection-of-two-arrays](https://github.com/rajenderthota/Leet-GFG/tree/master/0349-intersection-of-two-arrays) |
@@ -148,12 +149,14 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 ## Divide and Conquer
 |  |
 | ------- |
+| [0215-kth-largest-element-in-an-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0215-kth-largest-element-in-an-array) |
 | [0948-sort-an-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0948-sort-an-array) |
 ## Sorting
 |  |
 | ------- |
 | [0015-3sum](https://github.com/rajenderthota/Leet-GFG/tree/master/0015-3sum) |
 | [0088-merge-sorted-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0088-merge-sorted-array) |
+| [0215-kth-largest-element-in-an-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0215-kth-largest-element-in-an-array) |
 | [0217-contains-duplicate](https://github.com/rajenderthota/Leet-GFG/tree/master/0217-contains-duplicate) |
 | [0274-h-index](https://github.com/rajenderthota/Leet-GFG/tree/master/0274-h-index) |
 | [0349-intersection-of-two-arrays](https://github.com/rajenderthota/Leet-GFG/tree/master/0349-intersection-of-two-arrays) |
@@ -169,6 +172,7 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 ## Heap (Priority Queue)
 |  |
 | ------- |
+| [0215-kth-largest-element-in-an-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0215-kth-largest-element-in-an-array) |
 | [0407-trapping-rain-water-ii](https://github.com/rajenderthota/Leet-GFG/tree/master/0407-trapping-rain-water-ii) |
 | [0948-sort-an-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0948-sort-an-array) |
 | [1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid](https://github.com/rajenderthota/Leet-GFG/tree/master/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid) |
@@ -502,4 +506,8 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 |  |
 | ------- |
 | [1477-product-of-the-last-k-numbers](https://github.com/rajenderthota/Leet-GFG/tree/master/1477-product-of-the-last-k-numbers) |
+## Quickselect
+|  |
+| ------- |
+| [0215-kth-largest-element-in-an-array](https://github.com/rajenderthota/Leet-GFG/tree/master/0215-kth-largest-element-in-an-array) |
 <!---LeetCode Topics End-->
