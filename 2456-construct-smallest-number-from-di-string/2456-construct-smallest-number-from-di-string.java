237dd8ad53f03c7adf7e167943f6c75b36202637
Rajender Thota <java.rajender@gmail.com>
2025-02-17 23:00:37 -0500

    2456-CONSTRUCT-SMALLEST-NUMBER-FROM-DI-STRING Stats: Time: 1 ms (75.25%), Space: 41.8 MB (48.69%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -1,5 +1,43 @@
 class Solution {
-    public String smallestNumber(String pattern) {
+
+//using backtracking
+ public String smallestNumber(String pattern) {
+        boolean[] used = new boolean[10]; // To track used digits (1-9)
+        StringBuilder result = new StringBuilder();
+        backtrack(pattern, 0, new StringBuilder(), used, result);
+        return result.toString();
+    }
+
+    private boolean backtrack(String pattern, int index, StringBuilder current, boolean[] used, StringBuilder result) {
+        if (index == pattern.length() + 1) {
+            if (result.length() == 0 || current.toString().compareTo(result.toString()) < 0) {
+                result.setLength(0);
+                result.append(current);
+            }
+            return true;
+        }
+
+        for (int num = 1; num <= 9; num++) {
+            if (used[num]) continue;
+            if (index == 0 || (pattern.charAt(index - 1) == 'I' && current.charAt(index - 1) - '0' < num) ||
+                    (pattern.charAt(index - 1) == 'D' && current.charAt(index - 1) - '0' > num)) {
+                
+                used[num] = true;
+                current.append(num);
+                
+                if (backtrack(pattern, index + 1, current, used, result)) {
+                    return true;
+                }
+                
+                current.deleteCharAt(current.length() - 1);
+                used[num] = false;
+            }
+        }
+        return false;
+    }
+
+
+    public String smallestNumber_stack(String pattern) {
          StringBuilder result = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
         
