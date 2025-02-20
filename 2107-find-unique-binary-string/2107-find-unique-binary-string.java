7c92503353be34e4f0cfc5ed353b879689c83c99
Rajender Thota <java.rajender@gmail.com>
2025-02-20 06:34:47 -0500

    2107-FIND-UNIQUE-BINARY-STRING Stats: Time: 13 ms (15.14%), Space: 42.3 MB (21.41%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.java
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -1,5 +1,26 @@
 class Solution {
-    public String findDifferentBinaryString(String[] nums) {
+
+
+ public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        Set<String> set = new HashSet<>();
+        for (String num : nums) {
+            set.add(num);
+        }
+        
+        for (int i = 0; i < (1 << n); i++) {
+            String candidate = String.format("%" + n + "s", Integer.toBinaryString(i)).replace(' ', '0');
+            if (!set.contains(candidate)) {
+                return candidate;
+            }
+        }
+        
+        return "";
+    }
+    
+
+    // most efficient way
+    public String findDifferentBinaryString_eff(String[] nums) {
          int n = nums.length;
         StringBuilder result = new StringBuilder();
         
