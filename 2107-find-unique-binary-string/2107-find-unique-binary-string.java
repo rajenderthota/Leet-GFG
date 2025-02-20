9610ca54905b732f7b3e6eb97f4143d12842c1c9
Rajender Thota <java.rajender@gmail.com>
2025-02-20 06:36:48 -0500

    2107-FIND-UNIQUE-BINARY-STRING Stats: Time: 1 ms (67.36%), Space: 41.9 MB (40.73%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.java
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -1,7 +1,45 @@
 class Solution {
 
 
- public String findDifferentBinaryString(String[] nums) {
+//using backtracking
+
+public String findDifferentBinaryString(String[] nums) {
+        Set<String> set = new HashSet<>();
+        for (String num : nums) {
+            set.add(num);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        return backtrack(set, sb, nums.length);
+    }
+    
+    private String backtrack(Set<String> set, StringBuilder sb, int n) {
+        if (sb.length() == n) {
+            String candidate = sb.toString();
+            if (!set.contains(candidate)) {
+                return candidate;
+            }
+            return null;
+        }
+        
+        sb.append('0');
+        String result = backtrack(set, sb, n);
+        if (result != null) return result;
+        sb.setLength(sb.length() - 1);
+        
+        sb.append('1');
+        result = backtrack(set, sb, n);
+        if (result != null) return result;
+        sb.setLength(sb.length() - 1);
+        
+        return null;
+    }
+
+//end of the backtracking
+
+
+//bruteforce
+ public String findDifferentBinaryString_bruteforce(String[] nums) {
         int n = nums.length;
         Set<String> set = new HashSet<>();
         for (String num : nums) {
