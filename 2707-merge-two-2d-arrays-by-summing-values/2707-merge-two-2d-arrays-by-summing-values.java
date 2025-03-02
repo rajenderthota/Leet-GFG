b0b615b749568a4dbceb6e2390025a3e6182b86e
Rajender Thota <java.rajender@gmail.com>
2025-03-02 09:05:01 -0500

    2707-MERGE-TWO-2D-ARRAYS-BY-SUMMING-VALUES Stats: Time: 0 ms (100%), Space: 45 MB (88.77%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,5 +1,43 @@
 class Solution {
+
+
+
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+
+        ArrayList<int[]> result=new ArrayList<>();
+        int n=nums1.length, m=nums2.length;
+        int i=0,j=0;
+
+        while( i< n && j < m){
+
+            if( nums1[i][0] == nums2[j][0]){
+                result.add( new int[]{nums1[i][0], nums1[i][1]+nums2[j][1]});
+                i++;j++;
+            }else if( nums1[i][0] < nums2[j][0]){
+                result.add( nums1[i]);
+                i++;
+            }else{
+                // result.add(new int[]{nums2[j][0],nums2[j][1]});
+                result.add(nums2[j]);
+                j++;
+            }
+        }
+
+        while(i < n){
+            result.add(nums1[i]);
+            i++;
+        }
+
+        while(j < m){
+            result.add(nums2[j]);
+            j++;
+        }
+
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public int[][] mergeArrays1(int[][] nums1, int[][] nums2) {
 
         // Arrays.sort(nums1,(a,b)->a[0]-b[0]);
         // Arrays.sort(nums2, (a,b)->a[0]-b[0]);
